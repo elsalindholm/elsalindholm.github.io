7540d92078fbe6bffc3c43703b836f6b465d3fa6
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-09-21 11:31:33 +0100

    About me section finished

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -8,29 +8,29 @@ export const AboutMe: React.FC = () => {
     <div className={'about-me'} id={'about-me'}>
       <h2 className={'about-me-title'}>ABOUT ME.</h2>
       <div className={'about-me-text-container'}>
-        <div className={'two-column'}>
-          <div className={'about-me-blurb'}>
-            <p>I am looking for work as a junior web developer. My skills include: </p>
-            <ul>
-              <li>HTML, CSS, JavaScript</li>
-              <li>TypeScript</li>
-              <li>React, MobX</li>
-              <li>Git, VSCode</li>
-            </ul>
-          </div>
-
-          <div className={'about-me-links'}>
-            <div className={'profile-picture'}></div>
-
-            <a href={'https://www.linkedin.com/in/elsalindholm/'} target={'_blank'}>
-              <div className={'linkedin'}></div>
-            </a>
-            <a>
-              <div className={'github'}></div>
-            </a>
-          </div>
+        <div className={'about-me-blurb'}>
+          <p>I am looking for work as a junior web developer. My skills include: </p>
+          <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>TypeScript</li>
+            <li>React, MobX</li>
+            <li>Git, VSCode</li>
+          </ul>
         </div>
-        <div>
+
+        <div className={'about-me-links'}>
+          <a href={'https://www.linkedin.com/in/elsalindholm/'} target={'_blank'}>
+            <div className={'linkedin'}></div>
+          </a>
+          <a href={'https://github.com/elsalindholm'} target={'_blank'}>
+            <div className={'github'}></div>
+          </a>
+          <a href={'https://www.codewars.com/users/elsa.lindholm'} target={'_blank'}>
+            <div className={'codewars'}></div>
+          </a>
+        </div>
+
+        <div className={'portfolio-blurb'}>
           <p>Find my latest portfolio projects below.</p>
         </div>
       </div>
